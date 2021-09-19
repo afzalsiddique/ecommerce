215142ab3b10b38d25eda14dc6e7f540bf3a6e85
Afzal Siddiqeu <afzal.siddique99@gmail.com>
2021-09-19 12:53:44 +0600

    create react app
    Product list is not static anymore. It shows all the product from an obj from data.js file.

---
+++ amazona/frontend/src/data.js
@@ -0,0 +1,71 @@
+const data ={
+  products:[
+    {
+      _id:'1',
+      name:'Nike Slim Shirt',
+      category: 'Shirts',
+      image:'/images/p1.jpg',
+      price:120,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:12,
+      description: 'high quality product'
+    },
+    {
+      _id:'2',
+      name:'Adidas Fit Shirt',
+      category: 'Shirts',
+      image:'/images/p2.jpg',
+      price:100,
+      brand:'Adidas',
+      rating:4.0,
+      numReviews:10,
+      description: 'high quality product'
+    },
+    {
+      _id:'3',
+      name:'Lacoste Free Shirt',
+      category: 'Shirts',
+      image:'/images/p3.jpg',
+      price:120,
+      brand:'Lacoste',
+      rating:5.0,
+      numReviews:17,
+      description: 'high quality product'
+    },
+    {
+      _id:'4',
+      name:'Nike Slim Pant',
+      category: 'Pant',
+      image:'/images/p4.jpg',
+      price:110,
+      brand:'Nike',
+      rating:4.5,
+      numReviews:19,
+      description: 'high quality product'
+    },
+    {
+      _id:'5',
+      name:'Adidas Fit Pant',
+      category: 'Pant',
+      image:'/images/p5.jpg',
+      price:190,
+      brand:'Puma',
+      rating:3.5,
+      numReviews:18,
+      description: 'high quality product'
+    },
+    {
+      _id:'6',
+      name:'Lacoste Fit Pant',
+      category: 'Pant',
+      image:'/images/p6.jpg',
+      price:65,
+      brand:'Lacoste',
+      rating:2.5,
+      numReviews:21,
+      description: 'high quality product'
+    }
+]
+}
+export default data;
