43617bc7c48911dbf18208513d99e95df9c88daa
Afzal Siddiqeu <afzal.siddique99@gmail.com>
2021-09-19 20:26:31 +0600

    Create Product Page

--- amazona/frontend/src/components/Product.js
+++ amazona/frontend/src/components/Product.js
@@ -1,8 +1,9 @@
+import React from "react";
 import Rating from "./Rating";
 
-export default function Product(props){
-  const {product} = props;
-  return(
+export default function Product(props) {
+  const { product } = props;
+  return (
     <div key={product._id} className="card">
       <a href={`/product/${product._id}`}>
         <img className="medium" src={product.image} alt={product.name} />
@@ -11,9 +12,9 @@ export default function Product(props){
         <a href={`/product/${product._id}`}>
           <h2>{product.name}</h2>
         </a>
-        <Rating rating={product.rating} numReviews={product.numReviews}/>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
         <div className="price">${product.price}</div>
       </div>
     </div>
-  )
+  );
 }
