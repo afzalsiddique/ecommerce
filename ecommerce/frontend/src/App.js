517892b049477582cc66c21224af4bbc9641d2e5
Afzal Siddique <afzal.siddique99@gmail.com>
2021-09-22 10:20:49 +0600

    create place order page in the front end

--- ecommerce/frontend/src/App.js
+++ ecommerce/frontend/src/App.js
@@ -5,6 +5,7 @@ import { signout } from './actions/userActions';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
@@ -38,7 +39,7 @@ function App() {
             {userInfo ? (
               <div className="dropdown">
                 <Link to="#">
-                  {userInfo.name} <i className="fa fa-caret-down"/>{' '}
+                  {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
                 </Link>
                 <ul className="dropdown-content">
                   <li>
@@ -60,6 +61,7 @@ function App() {
           <Route path="/register" component={RegisterScreen}/>
           <Route path="/shipping" component={ShippingAddressScreen}/>
           <Route path="/payment" component={PaymentMethodScreen}/>
+          <Route path="/placeorder" component={PlaceOrderScreen}/>
           <Route path="/" component={HomeScreen} exact/>
         </main>
         <footer className="row center">All right reserved</footer>
