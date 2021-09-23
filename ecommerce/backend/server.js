26f101b80ec76984f831d2fa5ea80fa85b2fa371
Afzal Siddique <afzal.siddique99@gmail.com>
2021-09-23 15:41:17 +0600

    create admin drop down

--- ecommerce/backend/server.js
+++ ecommerce/backend/server.js
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).send({message: err.message})
 })
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
